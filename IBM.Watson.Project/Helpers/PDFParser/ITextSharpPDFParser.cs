a2f4b51fd0f2d15de81901d1b208fa08e8083165
Luka Cvetkovic <cvetko911@hotmail.com>
2016-12-13 20:24:45 +0100

    Pdf parser

---
+++ IBM.Watson.Project/Helpers/PDFParser/ITextSharpPDFParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace IBM.Watson.Project.Helpers.PDFParser
+{
+    public class ITextSharpPDFParser : IPDFParser
+    {
+        public string ExtractTextFromPdf(string path)
+        {
+            return ExtractTextFromPdfBetter(path);
+        }
+
+        public string ExtractTextFromPdfWorse(string path)
+        {
+            using (PdfReader reader = new PdfReader(path))
+            {
+                StringBuilder text = new StringBuilder();
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+
+                return text.ToString();
+            }
+        }
+
+        public string ExtractTextFromPdfBetter(string path)
+        {
+            ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+
+            using (PdfReader reader = new PdfReader(path))
+            {
+                StringBuilder text = new StringBuilder();
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    string thePage = PdfTextExtractor.GetTextFromPage(reader, i, its);
+                    string[] theLines = thePage.Split('\n');
+                    foreach (var theLine in theLines)
+                    {
+                        text.AppendLine(theLine);
+                    }
+                }
+                return text.ToString();
+            }
+        }
+    }
+
+   
+}
--- IBM.Watson.Project/Program.cs
+++ IBM.Watson.Project/Program.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using IBM.Watson.Project.Helpers;
+using IBM.Watson.Project.Helpers.PDFParser;
 using IBM.Watson.Project.Helpers.WatsonHelper;
 
 namespace IBM.Watson.Project
@@ -14,20 +15,24 @@ namespace IBM.Watson.Project
         private static readonly string APIKey = "ea4cb11ced2395985e21914549b176c68111faac";
         static void Main(string[] args)
         {
+            IPDFParser pdfParser = new ITextSharpPDFParser();
+
+            var text = pdfParser.ExtractTextFromPdf("test.pdf");
+
             IWatsonHelper helper = new WatsonHelperText(APIKey);
 
-            var result = helper.GetResult("Ove je srijede, 30. studenoga, dovršena velika operacija koju su zajednički provodile policije i tužiteljstva Njemačke i SAD-a, FBI, Eurojust, Europol, kao i stručnjaci iz 30 zemalja, a u kojoj je uspješno ugašena velika botnet struktura naziva Avalanche. Ova je mreža botova još od 2009. godine služila za distribuciju zloćudnih programa, phishing napade i regrutiranje ljudi u razne malverzacije, tj. njihovo pretvaranje u financijske mule.\r\n \r\nŠtete prouzročene putem ovog sustava procjenjuju se na više stotina milijuna eura, a samo u Njemačkoj bankovni su sektor izravno koštale oko 6 milijuna eura. Putem Avalanche mreže napadači su tjedno prosječno žrtvama slali preko milijun zaraženih ili phishing e-mailova.\r\n \r\nEuropolova istraga ovog slučaja započela je 2012. godine u Njemačkoj, a ovotjedna je akcija rezultirala uhićenjem pet osoba, te zapljenom 39 servera u istom danu. Dodatno, više od 800.000 domena koje su služile za distribuciju ilegalnih programa i lansiranje napada je ugašeno, zaplijenjeno ili blokirano tijekom ove policijske operacije, javlja Europol.\r\n \r\nAkcija suzbijanja ove malware mreže bila je iznimno zahtjevna jer su svi serveri bili zaštićeni sofisticiranom tehnikom \"double fast flux\" koja vrlo brzo mijenja DNS zapise i promet preusmjerava kroz višestruke proxyje, te tako skriva tragove napadača.");
+            var result = helper.GetResult(text);
             Console.WriteLine(JSONHelper.Serialize(result));
 
             Console.WriteLine();
             Console.WriteLine("-- ------------------------------- --");
             Console.WriteLine();
 
-            IWatsonHelper helpUrl = new WatsonHelperUrl(APIKey);
+            //IWatsonHelper helpUrl = new WatsonHelperUrl(APIKey);
 
-            var resultUrl= helpUrl.GetResult("http://www.baynews9.com/alerts.html");
+            //var resultUrl= helpUrl.GetResult("http://www.baynews9.com/alerts.html");
 
-            Console.WriteLine(JSONHelper.Serialize(result));
+            //Console.WriteLine(JSONHelper.Serialize(result));
 
             Console.ReadLine();
         }
